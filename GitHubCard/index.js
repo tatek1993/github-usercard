995d51c6fad913b81db38480e23a2d43f3a51792
Tate Keller <tatek1993@gmail.com>
2020-01-16 17:15:34 -0600

    appended, created text content, created array of followers, used for each to loop through followers

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,9 +2,13 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+const entryPoint = document.querySelector(".cards");
+
 axios.get("https://api.github.com/users/tatek1993")
 .then(response => {
-  console.log(response);
+  console.log(response.data);
+ 
+  entryPoint.append(ghProfile(response.data))
 
 })
 
@@ -32,7 +36,14 @@ axios.get("https://api.github.com/users/tatek1993")
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['Keyeric', 'seanaleid', 'anthonyj713', 'shellcupp', 'ForestJohnson', 'lgv-0', 'tdefriess', 'Reikiryo'];
+
+  followersArray.forEach( username => {
+    axios.get(`https://api.github.com/users/${username}`)
+    .then(response => {
+      entryPoint.append(ghProfile(response.data))
+    })
+  })
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -66,17 +77,42 @@ function ghProfile(profile) {
         userFollowing = document.createElement("p"),
         userBio = document.createElement("p");
 
-  userImg.src = profile;
-  fullName.textContent = 
-  userName.textContent =
-  userLocation.textContent = `Location: $profile.location`;       
+  
+
+  userImg.src = profile.avatar_url;
+  fullName.textContent = profile.name;
+  userName.textContent = profile.login;
+  userLocation.textContent = `Location: ${profile.location}`;
+  userGhLink.textContent = profile.html_url;
+  userGhLink.href = profile.html_url;
+  userProfile.textContent = 'Profile:'; 
+  userFollowers.textContent = `Followers: ${profile.followers}`;
+  userFollowing.textContent = `Following: ${profile.following}`;
+  userBio.textContent = `Bio: ${profile.bio}`;     
 
   newCard.classList.add("card");
   cardInfo.classList.add("card-info");
   fullName.classList.add("name");
   userName.classList.add("username");
 
+  newCard.append(userImg);
+  newCard.append(cardInfo);
+  cardInfo.append(fullName);
+  cardInfo.append(userName);
+  cardInfo.append(userLocation);
+  cardInfo.append(userProfile);
+  userProfile.append(userGhLink);
+  cardInfo.append(userFollowers);
+  cardInfo.append(userFollowing);
+  cardInfo.append(userBio);
+
+  return newCard;
+
 }
+
+
+
+
 
 /* List of LS Instructors Github username's: 
   tetondan
